a3284dd4529e3ba00bbebb398c80fce7eeb19b84
ronin182 <44127288+ronin182@users.noreply.github.com>
2018-10-21 20:26:32 +0200

    Update NewToken.sol

--- NewToken.sol
+++ NewToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.11;
 import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
-	function NewToken() {
+	function NewTok44en() {
 		totalSupply = 123;
 		name = "Best ever coin";
 		decimals = 4;
